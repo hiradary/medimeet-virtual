e8c8d0932e8f92e79176774c5d1e277d8c8e21af
hiradary <hiradwork@gmail.com>
2024-03-03 14:38:48 -0800

    Initial commit

---
+++ src/main/java/com/example/medimeet/MedimeetApplication.java
@@ -0,0 +1,13 @@
+package com.example.medimeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedimeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedimeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medimeet/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medimeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedimeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
