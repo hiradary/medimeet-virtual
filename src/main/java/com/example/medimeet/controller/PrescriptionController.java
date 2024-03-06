66f7bec5aebd0053bb5dab6e26c5689ad44efaa2
krisha mahat <krishamahat751@gmail.com>
2024-03-06 13:25:43 -0800

    prescription CRUD

---
+++ src/main/java/com/example/medimeet/controller/PrescriptionController.java
@@ -0,0 +1,5 @@
+package com.example.medimeet.controller;
+
+public class PrescriptionController {
+
+}
--- src/main/java/com/example/medimeet/model/PrescriptionRepository.java
+++ src/main/java/com/example/medimeet/model/PrescriptionRepository.java
@@ -1,5 +1,12 @@
 package com.example.medimeet.model;
 
-public interface PrescriptionRepository {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
+	Optional<Prescription> findById(Long id);
+	List<Prescription> findBypatient(Long id);
+	
 }
