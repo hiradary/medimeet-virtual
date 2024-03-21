c6208ea8a1ab3e72d96a9e733d86966eaf688f44
Hirad Arshadi <hiradwork@gmail.com>
2024-03-20 20:46:14 -0700

    Remove everything

--- src/main/java/com/example/medimeet/MedimeetApplication.java
+++ src/main/java/com/example/medimeet/MedimeetApplication.java
@@ -7,14 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.example.medimeet.repositories.AppointmentRepository;
-import com.example.medimeet.model.Doctor;
-import com.example.medimeet.repositories.DoctorRepository;
-import com.example.medimeet.repositories.FeedbackRepository;
-import com.example.medimeet.repositories.PrescriptionRepository;
-import com.example.medimeet.model.User;
-import com.example.medimeet.repositories.UserRepository;
-
 @SpringBootApplication
 public class MedimeetApplication {
 
@@ -23,34 +15,18 @@ public class MedimeetApplication {
 	}
 
     @Bean
-    ApplicationRunner init(DoctorRepository doctorRepository,
-                                  AppointmentRepository appointmentRepository, UserRepository userRepository, FeedbackRepository feedbackRepository,
-                                  PrescriptionRepository prescriptionRepository) {
+    ApplicationRunner init() {
         return args -> {
             
-            loadData(doctorRepository, appointmentRepository, userRepository, feedbackRepository, prescriptionRepository);
+            
         };
     }
 	
 	
-	
-	
-	private void loadData(DoctorRepository doctorRepository, 
-    		AppointmentRepository appointmentRepository, UserRepository userRepository,FeedbackRepository feedbackRepository, 
-    		PrescriptionRepository prescriptionRepository) {
-		       User user = new User();
-		       LocalDateTime currentDateTime = LocalDateTime.now();
-       
-		 
-		        ArrayList<Doctor> doctors = new ArrayList<>();
-		        doctors.add(new Doctor(user, "Cardiology", currentDateTime, "Experienced cardiologist specializing in heart diseases.", "doctor1.jpg"));  
-		        doctors.add(new Doctor(user, "Dermatology", currentDateTime, "Dermatologist with a focus on skincare treatments.", "doctor2.jpg"));
-		        doctors.add(new Doctor(user, "Endocrinology", currentDateTime, "Endocrinologist dedicated to hormone-related disorders.", "doctor3.jpg"));
-		        doctors.add(new Doctor(user, "Gastroenterology", currentDateTime, "Gastroenterologist providing expert care for digestive issues.", "doctor4.jpg"));
-		        doctors.add(new Doctor(user, "Hematology", currentDateTime, "Hematologist specializing in blood disorders.", "doctor5.jpg"));
-		        doctorRepository.saveAll(doctors);
+	private void loadData() {
+
 	}
-			}
+}
 	
 	
 	
--- src/main/java/com/example/medimeet/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.medimeet.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.medimeet.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
