4cd274c284c74413153b32a3b4597e3dc2ca3fa4
Hirad Arshadi <hiradwork@gmail.com>
2024-03-21 19:54:30 -0700

    Remove unused imports

--- src/main/java/com/example/medimeet/MedimeetApplication.java
+++ src/main/java/com/example/medimeet/MedimeetApplication.java
@@ -1,7 +1,5 @@
 package com.example.medimeet;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList; 
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
